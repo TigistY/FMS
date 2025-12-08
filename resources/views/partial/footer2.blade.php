4a1e5d5ef70fd9f2742343bfb4650c7cd3a23ac1
TigistY <tg.yeshambel.cs@gmail.com>
2025-12-09 01:47:10 +0300

    the next stage

--- resources/views/partial/footer2.blade.php
+++ resources/views/partial/footer2.blade.php
@@ -10,6 +10,7 @@
           		<li><a class="text-white offset-1"  href="#">information</a></li>
           		<li><a class="text-white offset-1"  href="#">Contact us</a></li>
 					<li><a class="text-white offset-1"  href="#">tttt</a></li>
+					<li><a class="text-white offset-1"  href="#">tttt</a></li>
           	</ul>
           </div>
          <div class="col-sm-3">
